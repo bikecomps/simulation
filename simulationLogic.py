67495708d2a169b7169f99bd621f2ad997acfdc8
Mikenna <mikenna.everett@gmail.com>
2013-10-28 21:04:12 -0500

    datetimes/trips working, still need to connect
    Still need to connect to the database. Currently contains its own Trip
    class so as not to depend on data_model.py

--- simulationLogic.py
+++ simulationLogic.py
@@ -1,15 +1,20 @@
 '''
     TO DO:
-    - Test that datetimes were done correctly
-    - Test use of Trip objects
     - connect to database
 '''
-import data_model
+# import data_model
 import Queue
 import random
 import datetime
+# # Might need to move this to simulator eventually
+# import hidden
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+DEPARTURE_TYPE = 0
+ARRIVAL_TYPE = 1
 
 class SimulationLogic:
+
     def __init__(self):
         self.trip_list = []
         # Pending departures/arrivals includes tuples (endTime, trip)
@@ -23,7 +28,15 @@ class SimulationLogic:
         # list of trips that didn't end at the desired station due to a dock shortage
         self.dock_shortages = []
         self.bike_shortages = []
-        
+
+        # Might need to move all this to simulator eventually
+        # engine_path = 'postgresql://%s:%s@localhost/%s' % (hidden.DB_USERNAME, hidden.DB_PASSWORD, hidden.DB_NAME)
+
+        # # echo=False is unnecessary but setting it to True provides useful info.
+        # engine = create_engine(engine_path, echo=False)
+        # Session = sessionmaker(bind=engine)
+        # self.session = Session()
+                
     def initialize(self, start_time):
         '''Sets states of stations at the start_time'''
         self.time = start_time
@@ -52,11 +65,11 @@ class SimulationLogic:
             print "STATION", station, "HAS", self.stations[station], "BIKES"
             for i in range(num_trips):
                 end_station_ID = random.choice(self.stations.keys())
-                start_time = self.time + datetime.timedelta(minutes=random.randint(0, timestep))
+                start_time = self.time + datetime.timedelta(minutes=random.randint(0, timestep.total_seconds()/60))
                 # Nobody takes longer than 2 hours to bike anywhere, duh!
                 end_time = start_time + datetime.timedelta(minutes=random.randint(0, 120))
                 # new_trip = [station, end_station_ID, start_time, end_time]
-                new_trip =  data_model.Trip(str(random.randint(1,500)), "Casual", "Produced", start_time, end_time, station, end_station_ID)
+                new_trip =  Trip(str(random.randint(1,500)), "Casual", "Produced", start_time, end_time, station, end_station_ID)
 
                 self.pending_departures.put((start_time, new_trip))
 
@@ -64,7 +77,7 @@ class SimulationLogic:
 
         # TESTING
         printedQueue = Queue.PriorityQueue()
-        print "PENDING TRIPS at time %d:" % (self.time)
+        print "PENDING TRIPS at time %s:" % str(self.time)
         for i in range(self.pending_arrivals.qsize()):
             pq_trip = self.pending_arrivals.get()
             print pq_trip
@@ -79,7 +92,7 @@ class SimulationLogic:
             self.bike_shortages.append(trip)
         else:
             self.stations[departure_station_ID] -= 1
-            self.pending_arrivals.put((trip.end_time, trip))
+            self.pending_arrivals.put((trip.end_date, trip))
 
             
 
@@ -105,10 +118,10 @@ class SimulationLogic:
             eventType = None
         elif self.pending_arrivals.empty():
             trip = self.pending_departures.get()[1]
-            eventType = 2
+            eventType = DEPARTURE_TYPE
         elif self.pending_departures.empty():
             trip = self.pending_arrivals.get()[1]
-            eventType = 3
+            eventType = ARRIVAL_TYPE
         else:
             first_departure = self.pending_departures.get()[1]
             first_arrival = self.pending_arrivals.get()[1]
@@ -116,11 +129,11 @@ class SimulationLogic:
             print "Departure", first_departure.start_date, "Arrival", first_arrival.end_date
             if (first_departure.start_date <= first_arrival.end_date):
                 trip = first_departure
-                eventType = 2
+                eventType = DEPARTURE_TYPE
                 self.pending_arrivals.put((first_arrival.end_date, first_arrival))
             else:
                 trip = first_arrival
-                eventType = 3
+                eventType = ARRIVAL_TYPE
                 self.pending_departures.put((first_departure.start_date, first_departure))
         return (eventType, trip)
 
@@ -131,38 +144,40 @@ class SimulationLogic:
         eventType = eventTuple[0]
         trip = eventTuple[1]
         while eventType != None:
-            if trip[eventType] >= self.time:
-                # Put trip back in proper queue if it's over the time, and stop the while loop
-                if eventType == 2:
-                        self.pending_departures.put((trip.start_date, trip))
-                elif eventType == 3:
-                        self.pending_arrivals.put((trip.end_date, trip))
-                break
-            if eventType == 2:
-                print "Resolving departure for", trip
-                self.resolve_departure(trip)
-            elif eventType == 3:
-                print "Resolving arrival for", trip
-                self.resolve_arrival(trip)
-                print "If dock shortage, new trip is", trip
+            if eventType == DEPARTURE_TYPE:
+                if trip.start_date > self.time:
+                    # Put trip back in proper queue if it's over the time, and stop the while loop
+                    self.pending_departures.put((trip.start_date, trip))
+                    break
+                else:
+                    print "Resolving departure for", trip
+                    self.resolve_departure(trip)
+            elif eventType == ARRIVAL_TYPE:
+                if trip.end_date > self.time:
+                    self.pending_arrivals.put((trip.end_date, trip))
+                    break
+                else:
+                    print "Resolving arrival for", trip
+                    self.resolve_arrival(trip)
+                    print "If dock shortage, new trip is", trip
 
             eventTuple = self.get_first_trip_event()
             eventType = eventTuple[0]
             trip = eventTuple[1]
             
         #TESTING
-        print "PENDING DEPARTURES at time %d:" % (self.time)
+        print "PENDING DEPARTURES at time %s:" % str(self.time)
         printedQueue = Queue.PriorityQueue()
         for i in range(self.pending_departures.qsize()):
             pq_trip = self.pending_departures.get()
-            print pq_trip
+            print pq_trip[1]
             printedQueue.put(pq_trip)
         self.pending_departures = printedQueue
-        print "PENDING ARRIVALS at time %d:" % (self.time)
+        print "PENDING ARRIVALS at time %s:" % str(self.time)
         printedQueue = Queue.PriorityQueue()
         for i in range(self.pending_arrivals.qsize()):
             pq_trip = self.pending_arrivals.get()
-            print pq_trip
+            print pq_trip[1]
             printedQueue.put(pq_trip)
         self.pending_arrivals = printedQueue
         print "RESOLVED TRIPS:"
@@ -178,3 +193,33 @@ class SimulationLogic:
 
     def cleanup(self):
         pass
+
+class Trip():
+    '''
+    Stores all trips from test data as well as created trip data
+    '''
+    def __init__(self, bike_id, member_type, trip_type, start_date, end_date,
+                 start_station_id, end_station_id):
+        self.bike_id = bike_id
+        self.member_type = member_type
+        self.trip_type = trip_type
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_station_id = start_station_id
+        self.end_station_id = end_station_id
+
+    def __str__(self):
+        return str(self.start_station_id) + " -> " + str(self.end_station_id) + ": " + str(self.start_date) + ", " + str(self.end_date)
+
+
+def main():
+    SL = SimulationLogic()
+    start_time = datetime.datetime(2013, 1, 1, 0, 0, 0)
+    SL.initialize(start_time)
+    step = datetime.timedelta(minutes=30)
+    SL.update(step)
+    SL.update(step)
+    SL.flush()
+
+if __name__ == "__main__":
+    main()
