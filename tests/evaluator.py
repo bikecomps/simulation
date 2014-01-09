632c2fc706592745fdf665cf403b39ca1004fcb9
Carlton <keedyc@carleton.edu>
2014-01-09 09:55:06 -0600

    Updated evaluator.py to query the DB for trips; removed camel-cased method and variable names.

--- tests/evaluator.py
+++ tests/evaluator.py
@@ -3,77 +3,92 @@
 A class for comparing the trips our simulator generates to real trips.
 '''
 
-from utils import Connector
+from utility import Connector
 
 class Evaluator:
-	def __init__(self):
-		self.trips = self.getTrips()
-		self.stationPairs = self.setupStationPairs(self.trips)
-	
-	def compareTripNumbers(self,stationPair):
-		numRealTrips = len(stationPair.realTrips)
-		numGendTrips = len(stationPair.gendTrips)
-		return numRealTrips - numGendTrips
-		
-	def compareAvgTripTimes(self,stationPair):
-		totalRealTripTime = 0
-		totalGendTripTime = 0
-		for trip in stationPair.realTrips:
-			tripLength = 1 # how should this be calculated?
-			totalRealTripTime += tripLength
-		for trip in stationPair.gendTrips:
-			tripLength = 1 # how should this be calculated?
-			totalGendTripTime += tripLength
-		avgRealTripTime = totalRealTripTime / len(stationPair.realTrips)
-		avgGendTripTime = totalGendTripTime / len(stationPair.gendTrips)
-		return avgRealTripTime - avgGendTripTime
-		
-	def setupStationPairs(self,trips):
-		stationPairs = {}
-		for trip in trips:
-			start = trip.getStart() # How do we work with trip objects?
-			end = trip.getEnd()
-			# if (start,end) not in stationPairs:
-				# newStationPair = StationPair(start,end)
-				# newStationPair.addTrip(trip)
-				# stationPairs[(start,end)] = newStationPair
-			# else:
-				# stationPairs[(start,end)] = stationPairs[(start,end)].addTrip(trip)
-		return stationPairs
-	
-	def getTrips(self):
-		trips = []
-		c = Connector()
+    def __init__(self):
+        self.trips = self.get_trips()
+        self.station_pairs = self.setup_station_pairs(self.trips)
+    
+    def compare_trip_numbers(self,station_pair):
+        num_real_trips = len(station_pair.real_trips)
+        num_prod_trips = len(station_pair.prod_trips)
+        return num_real_trips - num_prod_trips
+        
+    def compare_avg_trip_times(self,station_pair):
+        total_real_trip_time = 0
+        total_prod_trip_time = 0
+        for trip in station_pair.real_trips:
+            trip_length = trip.trip_length # how should this be calculated?
+            total_real_trip_time += trip_length
+        for trip in station_pair.prod_trips:
+            trip_length = trip.trip_length # how should this be calculated?
+            total_prod_trip_time += trip_length
+        avg_real_trip_time = total_real_trip_time / len(station_pair.real_trips)
+        avg_prod_trip_time = total_prod_trip_time / len(station_pair.prod_trips)
+        return avg_real_trip_time - avg_prod_trip_time
+        
+    def setup_station_pairs(self,trips):
+        station_pairs = {}
+        for trip in trips:
+            start = trip.start_station_id
+            end = trip.end_station_id
+            if (start,end) not in station_pairs:
+                new_station_pair = StationPair(start,end)
+                new_station_pair.add_trip(trip)
+                station_pairs[(start,end)] = new_station_pair
+            else:
+                station_pairs[(start,end)].add_trip(trip)
+        return station_pairs
+    
+    def get_trips(self,start_time,end_time):
+        c = Connector()
 		session = c.getDBSession()
     	engine = c.getDBEngine()
-    	for trip in session.query(Trip) \
-            .filter(Trip.type == 'production') \
-            .yield_per(cap):
-            # if there's a corresponding 'testing' trip in the databse:
-            # 	trips.append(trip)
-            # 	trips.append(the corresponding 'testing' trip)
+    	trips = session.query(Trip) \
+            # Will the filter below work?
+            .filter(Trip.trip_type == 'Produced' or Trip.trip_type == 'Testing') \
+            .filter(Trip.start_date.between(start_date, end_date)) \
+            .yield_per(cap)
         return trips
         
 class StationPair:
-	def __init__(self,start,end):
-		self.startStation = start
-		self.endStation = end
-		self.realTrips = []
-		self.gendTrips = []
-	
-	def addTrip(self,trip):
-		if trip.isReal(): # ???
-			self.realTrips.append(trip)
-		elif trip.isGend(): # ???
-			self.gendTrips.append(trip)
-		else:
-			print "Error: StationPair trips must be of type 'training' or 'product'."
-			
+    def __init__(self,start,end):
+        self.start_station = start
+        self.end_station = end
+        self.real_trips = []
+        self.prod_trips = []
+    
+    def add_trip(self,trip):
+        if trip.trip_type == 'Testing':
+            self.real_trips.append(trip)
+        elif trip.trip_type == 'Produced':
+            self.prodTrips.append(trip)
+        else:
+            print "Error: station_pair trips must be of type 'Testing' or 'Produced'."
+            
 def main():
-	evaluator = Evaluator()
-	for stationPair in evaluator.stationPairs:
-		tripNumDiff = evaluator.compareTripNumbers(stationPair)
-		print 'StationPair', stationPair, 'generated', tripNumDiff, 'fewer trips than reality.'
-
+    evaluator = Evaluator()
+    for pair in evaluator.station_pairs:
+        # NOTE: Encapsulate the section below into a method in Evaluator.
+        # NOTE: It could be helpful to note particularly high or low differences.
+        num_diff = evaluator.compare_trip_numbers(pair)
+        if num_diff > 0:
+            print 'station_pair %s generated %d fewer trips than reality.' % (pair, num_diff)
+        elif num_diff < 0:
+            print 'station_pair %s generated %d more trips than reality.' % (pair, num_diff)
+        else:
+            print 'station_pair %s generated exactly the real number of trips!' % pair
+    
+    # NOTE: Consider encapsulating the section below into a method in Evaluator.
+    #for pair in evaluator.station_pairs:
+    #    avgDiff = evaluator.compare_avg_trip_times(pair)
+    #    if avgDiff > 0:
+    #        print 'foo'
+    #    elif avgDiff < 0:
+    #        print 'bar'
+    #    else:
+    #        print 'baz'
+            
 if __name__ == '__main__':
-	main()
+    main()
